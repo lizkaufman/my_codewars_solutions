da004d3e5a8e64ffcc8ad3bc7e063f8e3fd30c69
Liz Kaufman <lizkaufman92@gmail.com>
2023-03-02 18:01:42 +0000

    TS exercism - grade school

--- TS/Exercism/01.03.23_grade-school.ts
+++ TS/Exercism/01.03.23_grade-school.ts
@@ -16,34 +16,60 @@ Note that all our students only have one name. (It's a small town, what do you w
 */
 
 export class GradeSchool {
-  students: {};
+  private students: Record<number, string[]>;
+
   constructor() {
     this.students = {};
   }
 
-  roster() {
-    return this.students;
+  public roster() {
+    const currentList = Object.entries(this.students);
+    const deepCopy = currentList.map(([grade, names]) => {
+      return [grade, [...names]];
+    });
+    const roster = Object.fromEntries(deepCopy);
+    return roster;
   }
 
-  add(name: string, grade: number) {
+  public add(name: string, grade: number) {
     const existingNames = Object.values(this.students).flat();
     if (existingNames.includes(name)) {
-      //TODO: take the name out from the prev grade before adding it to the new grade using the logic below
+      const rosterArray = Object.entries(this.students);
+      for (let i: number = 0; i < rosterArray.length; i++) {
+        if (rosterArray[i].flat().includes(name)) {
+          const existingGrade = Number(rosterArray[i][0]);
+          const currentGradeStudents = Array.from(this.students[existingGrade]);
+          const indexToRemove = currentGradeStudents.indexOf(name);
+          const newGradeStudents = [
+            ...currentGradeStudents.slice(0, indexToRemove),
+            ...currentGradeStudents.slice(indexToRemove + 1),
+          ];
+          this.students = {
+            ...this.students,
+            [existingGrade]: newGradeStudents,
+          };
+          break;
+        }
+      }
+      this.add(name, grade);
     }
 
-    if (Object.keys(this.students).includes(grade + "")) {
-      const newGradeList = [
-        ...this.students[grade as keyof typeof this.students],
-        name,
-      ];
-      this.students = { ...this.students, [grade]: newGradeList.sort() };
+    if (Object.keys(this.students).includes(String(grade))) {
+      const newGradeList = [...this.students[grade], name];
+      this.students = {
+        ...this.students,
+        [grade]: newGradeList.sort(),
+      };
       return;
     }
-    this.students = { ...this.students, [grade]: [name] };
+    this.students = {
+      ...this.students,
+      [grade]: [name],
+    };
   }
 
-  grade(grade: number) {
-    const gradeList = this?.students[grade as keyof typeof this.students];
-    return gradeList ? gradeList : [];
+  public grade(grade: number) {
+    const gradeList = this.students[grade];
+    return gradeList ? [...gradeList] : [];
   }
 }
